a47a9cd58f4339ef1e160ecab21e500bab223507
sealionkat <sealionkat@gmail.com>
2017-01-15 15:32:57 +0100

    Added implementation of necessary API for bot.

--- game/js/botapi/botapi.js
+++ game/js/botapi/botapi.js
@@ -1,6 +1,9 @@
 me.Botapi = me.Object.extend({
+  init: function() {
+    console.warn('bot api');
+  },
   getBoard: function() {
-
+    return me.video.renderer.getContext2d(me.video.renderer.canvas).getImageData(0, 0, 480, 800);
   },
   pressLeftKey: function() {
     me.input.triggerKeyEvent(me.input.KEY.LEFT, true);
